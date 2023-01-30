41a32ad93983060b553a4d93dc89b2f7f988d3ae
tomix89 <84495269+tomix89@users.noreply.github.com>
2023-01-30 15:25:26 +0100

    do not rotate screen when mouse is on sliders

--- Assets/scripts/RotationControl.cs
+++ Assets/scripts/RotationControl.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.EventSystems;
 using UnityEngine;
 
 public class RotationControl : MonoBehaviour {
@@ -7,6 +6,12 @@ public class RotationControl : MonoBehaviour {
     private Camera cam;
     [SerializeField] private float sensitivity = 0.3f;
     Vector3 mouseDownPos = Vector3.zero;
+    bool canRotate = true;
+
+    // https://www.youtube.com/watch?v=ptmum1FXiLE
+    bool IsMouseOverUI() {
+        return EventSystem.current.IsPointerOverGameObject();
+    }
 
     // Update is called once per frame
     void Update() {
@@ -14,11 +19,16 @@ public class RotationControl : MonoBehaviour {
         // register the point where the mouse was clicked
         if (Input.GetMouseButtonDown(0)) {
             mouseDownPos = Input.mousePosition;
+
+            // we need to check this upon mouse button down
+            // as user might be able to hover off the UI item and the rotation would begin
+            canRotate = !IsMouseOverUI();
+
          //   print("mouseDownPos down at: " + mouseDownPos);
         }
 
         // and while it is down move by the current delta
-        if (Input.GetMouseButton(0)) {
+        if (Input.GetMouseButton(0) && canRotate) {
 
             Vector3 delta = Input.mousePosition - mouseDownPos;
 
