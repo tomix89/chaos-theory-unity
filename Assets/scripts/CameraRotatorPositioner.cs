ce26b1526b33cbd7db33901bbca858a08bbe52dd
tomix89 <84495269+tomix89@users.noreply.github.com>
2023-02-02 20:26:32 +0100

    New camera control
    - much smoother and less demanding calculation of rotation center

--- Assets/scripts/CameraRotatorPositioner.cs
+++ Assets/scripts/CameraRotatorPositioner.cs
@@ -4,44 +4,103 @@ using UnityEngine;
 
 public class CameraRotatorPositioner : MonoBehaviour {
 
-    private Dictionary<string, Vector3> averages = null;
-    private float coeff = 0.9999f;
+    private float decayCoeff = 0.00005f;
+    private float maxCamSpeed = 0.05f;
     bool wasUpdate = false;
 
+    private struct MMItem {
+        private string tag;
+        public float max;
+        public float min;
+
+        public void setTag(string tag) {
+            this.tag = tag;
+        }
+
+        // this creates disturbing camera jumps when being assinged direclty
+        public void SetLimits(float value) {
+            if (value < min) {
+                min = value;
+            }
+            if (value > max) {
+                max = value;
+            }
+        }
+
+        public void applyDecay(float amount) {
+            float oldMin = min;
+
+            min += max*amount;
+            max += oldMin*amount;
+        }
+
+        public float getAvg() {
+            print(tag + "  " + min + "  " + max);
+            return (max + min) / 2.0f;
+        }
+    }
+
+    private struct Limits {
+        public MMItem mmx;
+        public MMItem mmy;
+        public MMItem mmz;
+
+        public void SetLimits(Vector3 pos) {
+            mmx.SetLimits(pos.x);
+            mmy.SetLimits(pos.y);
+            mmz.SetLimits(pos.z);
+        }
+
+        public void applyDecays(float amount) {
+            mmx.applyDecay(amount);
+            mmy.applyDecay(amount);
+            mmz.applyDecay(amount);
+        }
+
+        public Vector3 getAvg() {
+            return new Vector3(mmx.getAvg(), mmy.getAvg(), mmz.getAvg());
+        }
+    }
+
+    Limits limits;
+
     void Start() {
-        averages = new Dictionary<string, Vector3>();
+        limits = new Limits();
+        limits.mmx.setTag("x");
+        limits.mmy.setTag("y");
+        limits.mmz.setTag("z");
         BasicMovement.OnPositionUpdateAction += HandlePositionUpdateAction;
     }
 
     void HandlePositionUpdateAction(string name, Vector3 pos) {
-
         wasUpdate = true;
-        if (averages.ContainsKey(name)) {
-            Vector3 oldAvg = averages[name];
-            Vector3 newAvg = oldAvg * coeff + (1 - coeff) * pos;
-            averages[name] = newAvg;
-
-            print(name + " " + newAvg);
-        } else {
-            averages.Add(name, pos);
-        }
-
+        limits.SetLimits(pos);
     }
 
-    // Update is called once per frame
-    void Update() {
 
+    Vector3 limitedDelta(Vector3 val) {
+        val.x = Mathf.Clamp(val.x, -maxCamSpeed, maxCamSpeed);
+        val.y = Mathf.Clamp(val.y, -maxCamSpeed, maxCamSpeed);
+        val.z = Mathf.Clamp(val.z, -maxCamSpeed, maxCamSpeed);
+
+        print("limitedDelta: " + val);
+
+        return val;
+    } 
+
+    void Update() {
         if (wasUpdate) {
             wasUpdate = false;
 
-            Vector3 sum = Vector3.zero;
-            foreach (var item in averages.Values) {
-                sum += item;
-            }
-
-            Vector3 finalAvg = sum / averages.Count;
+            limits.applyDecays(decayCoeff);
+            Vector3 finalAvg = limits.getAvg();
             print("final avg: " + finalAvg);
-            transform.position = finalAvg;
+
+            Vector3 delta = finalAvg - transform.position;
+            print("delta: " + delta);
+
+            // this alters all axis, maybe it will be nice to compensate for the camera to rotator distance (z)
+            transform.position += limitedDelta(delta); 
         }
     }
 }
